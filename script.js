1f0c4e3e3f225122d4beb0f0524d0ee3ac022653
Muhammadsaid07 <muhammadsaidtursunov0@gmail.com>
2025-03-20 15:17:08 +0500

    Better css

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const body = document.getElementsByTagName("body")[0];
-body.style.backgroundColor = "red";
-function setColor(name){
-    body.style.backgroundColor = name;
-}
-function randomColor(){
-    const red = Math.round(Math.random() *255)
-    const green = Math.round(Math.random() *255)
-    const blue = Math.round(Math.random() *255)
+const body = document.body;
 
-    const color = `rgb(${red}, ${green} , ${blue})`
-    body.style.backgroundColor = color;
+function setColor(color) {
+    body.style.background = color; // Overwrites the gradient with a solid color
 }
-randomColor()
+
+function randomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const randomColor = `rgb(${red}, ${green}, ${blue})`;
+
+    body.style.background = randomColor; // Applies random solid color
+}
